84b6600f0d12e3868a6f84a1804c3dea7e076efe
nicola montecchio <nicola.montecchio@gmail.com>
2015-03-28 23:23:00 +0000

    something here, still not all ok

--- test1.cc
+++ test1.cc
@@ -29,37 +29,29 @@ static int patestCallback( const void *inputBuffer, void *outputBuffer,
   float *out = (float*)outputBuffer;
   unsigned int i;
   (void) inputBuffer; /* Prevent unused variable warning. */
-  for( i=0; i<framesPerBuffer; i++ )
-  {
-    *out++ = data->left_phase; /* left */
-    *out++ = data->right_phase; /* right */
-/* Generate simple sawtooth phaser that ranges between -1.0 and 1.0. */
-    data->left_phase += 0.01f;
-/* When signal reaches top, drop back down. */
-    if( data->left_phase >= 1.0f ) data->left_phase -= 2.0f;
-/* higher pitch so we can distinguish left and right. */
-    data->right_phase += 0.03f;
-    if( data->right_phase >= 1.0f ) data->right_phase -= 2.0f;
-  }
+ //  for( i=0; i<framesPerBuffer; i++ )
+//   {
+//     *out++ = data->left_phase; /* left */
+//     *out++ = data->right_phase; /* right */
+// /* Generate simple sawtooth phaser that ranges between -1.0 and 1.0. */
+//     data->left_phase += 0.01f;
+// /* When signal reaches top, drop back down. */
+//     if( data->left_phase >= 1.0f ) data->left_phase -= 2.0f;
+// /* higher pitch so we can distinguish left and right. */
+//     data->right_phase += 0.03f;
+//     if( data->right_phase >= 1.0f ) data->right_phase -= 2.0f;
+//   }
+  libpd_process_float(1, (float *) inputBuffer, (float *) outputBuffer);
   return 0;
 }
 
 
 #define SAMPLE_RATE (44100)
 
-// void pdmessage ( const char * source, const char * symbol, int argc, t_atom * argv )
-// {
-//   int i;
-//   printf ( "Received message from patch: \n" );
-//   printf ( "Source=%s\n", source );
-//   printf ( "Symbol=%s\n", symbol );
-//   printf ( "Number of Arguments=%d\n\n", argc );
-// /* TODO t_atom management. */
-// }
-
 void pdprint(const char *s) {
   printf("%s", s);
 }
+
 
 int main(int argc, char **argv)
 {
@@ -67,7 +59,8 @@ int main(int argc, char **argv)
   libpd_set_printhook(pdprint);
   // libpd_set_messagehook(pdmessage); //NOPE
   libpd_init();
-  int init_err = libpd_init_audio(0, 2, 44100);
+
+  int init_err = libpd_init_audio(0, 2, 44100); // 0 in, 2 out, 44kHz
   if(init_err != 0)
   {
     printf("audio init failed with code %d\n", init_err);
@@ -77,9 +70,38 @@ int main(int argc, char **argv)
   // try to open the test patch
   void *patch = libpd_openfile("libpdtestpatch.pd", ".");
   printf("patch file opened; handle: %d\n", patch);
-
   libpd_bang("fufi");
-  // libpd_bind("gnappo");
+
+  int pd_tick_size = libpd_blocksize();
+
+  printf("PD tick size: %d\n", pd_tick_size);
+
+  // alsa stuff
+  int deviceNum = atoi(argv[1]);
+  Pa_Initialize();
+
+
+  PaStreamParameters outputParameters;
+  bzero( &outputParameters, sizeof( outputParameters ) ); //not necessary if you are filling in all the fields
+  outputParameters.channelCount = 2;
+  outputParameters.device = deviceNum;
+  outputParameters.hostApiSpecificStreamInfo = NULL;
+  outputParameters.sampleFormat = paFloat32;
+  outputParameters.suggestedLatency = Pa_GetDeviceInfo(deviceNum)->defaultLowOutputLatency ;
+  outputParameters.hostApiSpecificStreamInfo = NULL; //See you specific host's API docs for info on using this field
+
+  PaStream *stream;
+  Pa_OpenStream( &stream, NULL, &outputParameters, SAMPLE_RATE, pd_tick_size,  paNoFlag, patestCallback, NULL);
+  Pa_StartStream( stream );
+
+
+  Pa_Sleep(3*1000);
+
+  Pa_StopStream( stream );
+  Pa_CloseStream( stream );
+
+
+
   libpd_closefile(patch);
 
 
@@ -88,56 +110,7 @@ int main(int argc, char **argv)
 
 }
 
-//   int deviceNum = atoi(argv[1]);
 
-//   int err;
-
-//   static paTestData data;
-
-//   err = Pa_Initialize();
-//   if( err != paNoError )
-//   {
-//     printf( "PortAudio error: %s\n", Pa_GetErrorText( err ) );
-//     return 1;
-//   }
-
-//   int numDevices;
-//   numDevices = Pa_GetDeviceCount();
-//   if( numDevices < 0 )
-//   {
-//     printf( "ERROR: Pa_CountDevices returned 0x%x\n", numDevices );
-//     err = numDevices;
-//   }
-//   printf("found %d devices\n", numDevices);
-
-//   for(int i=0; i<numDevices; i++ )
-//   {
-//     const PaDeviceInfo *deviceInfo;
-//     deviceInfo = Pa_GetDeviceInfo( i );
-//     printf("device %d: %s\n", i, deviceInfo->name);
-//   }
-
-//   PaStreamParameters outputParameters;
-//   bzero( &outputParameters, sizeof( outputParameters ) ); //not necessary if you are filling in all the fields
-//   outputParameters.channelCount = 2;
-//   outputParameters.device = deviceNum;
-//   outputParameters.hostApiSpecificStreamInfo = NULL;
-//   outputParameters.sampleFormat = paFloat32;
-//   outputParameters.suggestedLatency = Pa_GetDeviceInfo(deviceNum)->defaultLowOutputLatency ;
-//   outputParameters.hostApiSpecificStreamInfo = NULL; //See you specific host's API docs for info on using this field
-
-//   PaStream *stream;
-//   // PaError err;
-// /* Open an audio I/O stream. */
-//   err = Pa_OpenStream( &stream, NULL, &outputParameters, SAMPLE_RATE, 256,  paNoFlag, patestCallback, &data);
-
-//   err = Pa_StartStream( stream );
-
-//   Pa_Sleep(2*1000);
-
-//   err = Pa_StopStream( stream );
-
-//   err = Pa_CloseStream( stream );
 
 //   err = Pa_Terminate();
 //   if( err != paNoError )
