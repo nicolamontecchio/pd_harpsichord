f6d0eda724ea9533c522f14fc640301bb5a76961
nicola montecchio <nicola.montecchio@gmail.com>
2015-03-28 23:39:23 +0000

    clean

--- test1.cc
+++ test1.cc
@@ -7,25 +7,12 @@
 using namespace std;
 
 
-// typedef struct
-// {
-//   float left_phase;
-//   float right_phase;
-// } paTestData;
-
-
-/* This routine will be called by the PortAudio engine when audio is needed.
-   It may called at interrupt level on some machines so don't do anything
-   that could mess up the system like calling malloc() or free().
-*/
 static int patestCallback( const void *inputBuffer, void *outputBuffer,
 			   unsigned long framesPerBuffer,
 			   const PaStreamCallbackTimeInfo* timeInfo,
 			   PaStreamCallbackFlags statusFlags,
 			   void *userData )
 {
-/* Cast data passed through stream to our structure. */
-  paTestData *data = (paTestData*)userData;
   float *out = (float*)outputBuffer;
   libpd_process_float(1, NULL, out);
   return 0;
