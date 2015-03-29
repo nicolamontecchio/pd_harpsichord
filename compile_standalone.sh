8486a340dff01b21c32bcb025f286f2b1c404ae1
nicola montecchio <nicola.montecchio@gmail.com>
2015-03-29 15:02:08 -0400

    list audio and midi devices

--- compile_standalone.sh
+++ compile_standalone.sh
@@ -1,2 +1,4 @@
 #c++ -o test1 -lportaudio -I../soundtest/libpd/libpd_wrapper -I../soundtest/libpd/pure-data/src -L../soundtest/libpd/libs -lpd  test1.cc
 c++ -o standalone  -lportaudio -Ilibpd/libpd_wrapper -Ilibpd/pure-data/src -Llibpd/libs  -lpd  standalone.c
+
+cc -o listdevices -lportaudio -lportmidi -O2 listdevices.c
