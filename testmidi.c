1db2757f8d405aa104b839a2b23ffd490f40b70f
nicola montecchio <nicola.montecchio@gmail.com>
2015-04-04 16:07:47 -0400

    note on/off detected

--- testmidi.c
+++ testmidi.c
@@ -3,6 +3,8 @@
 #include <portmidi.h>
 
 #define BUFLEN (100)
+#define MIDINOTEOFF 0x80
+#define MIDINOTEON 0x90
 
 int main(int argc, char **argv)
 {
@@ -19,8 +21,26 @@ int main(int argc, char **argv)
       int nread = Pm_Read(input_stream, msg_buffer, BUFLEN);
       for(int i = 0; i < nread; i++)
       {
-	printf("timestamp: %d -- message: %d\n", msg_buffer[i].timestamp, msg_buffer[i].message);
+	PmEvent msg = msg_buffer[i];
+	int status = Pm_MessageStatus(msg.message);
+	int data1  = Pm_MessageData1(msg.message);
+	int data2  = Pm_MessageData2(msg.message);
+	int data3  = ((msg.message >> 24) & 0xff);
+	int msgtype = ((status & 0xf0) == 0xf0 ?
+		       status : (status & 0xf0));
 
+	/* printf("timestamp: %d -- message: %d\n", msg_buffer[i].timestamp, msg_buffer[i].message); */
+	switch(msgtype)
+	{
+	case MIDINOTEON:
+	  printf("note on:  %d %d\n", data1, data2);
+	  break;
+	case MIDINOTEOFF:
+	  printf("note off: %d %d\n", data1, data2);
+	  break;
+	default:
+	  printf("wtf\n");
+	}
       }
     }
   }
