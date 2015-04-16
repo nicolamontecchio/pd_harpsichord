11b0536334843653f2d2e28c6c91eca865c7e45c
nicola montecchio <nicola.montecchio@gmail.com>
2015-04-16 10:40:50 +0100

    util to check if expr

---
+++ testpatch.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <strings.h>
+#include <z_libpd.h>
+
+
+void pdprint(const char *s) {
+  printf("%s", s);
+}
+
+int main(int argc, char **argv)
+{
+  if(argc == 2)
+  {
+    libpd_set_printhook(pdprint);
+    libpd_init();
+    void *patch = libpd_openfile(argv[1], ".");
+    printf("patch file opened; handle: %d\n", patch);
+    libpd_closefile(patch);
+    return 0;
+  }
+  else
+  {
+    printf("usage: %s [patchname].pd\n", argv[0]);
+    return 1;
+  }
+}
