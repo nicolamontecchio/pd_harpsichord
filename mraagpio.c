136013de1d8ec127dce21d2438a18bd50b9e48d3
nicola montecchio <nicola.montecchio@gmail.com>
2015-04-30 22:27:15 -0400

    clock scheduling functions in pd ext

--- mraagpio.c
+++ mraagpio.c
@@ -1,39 +1,48 @@
+#include <stdlib.h>
 #include "m_pd.h"
 
 
-
-/* -------------------------- mraagpio ------------------------------ */
 static t_class *mraagpio_class;
 
 typedef struct _mraagpio
 {
   t_object x_obj;
   t_clock *x_clock;
+  int *pins;
+  int n_pins;
 } t_mraagpio;
 
 void mraagpio_tick(t_mraagpio *x)
 {
-  /* post("tick"); */
-  /* clock_delay(x->x_clock, 1000); */
+  post("tick");
 
+  clock_delay(x->x_clock, 100);
 }
 
 void mraagpio_free(t_mraagpio *x)
 {
   clock_free(x->x_clock);
+  free(x->pins);
 }
 
-void *mraagpio_new(t_symbol *unitname, t_floatarg f, t_floatarg tempo)
+void *mraagpio_new(t_symbol *s, int argc, t_atom *argv)
 {
   t_mraagpio *x = (t_mraagpio *)pd_new(mraagpio_class);
-  /* mraagpio_ft1(x, f); */
-  /* x->x_hit = 0; */
+  x->n_pins = argc;
+  x->pins = malloc(sizeof(int) * argc);
+  for(int i = 0; i < argc; i++)
+  {
+    if(argv[i].a_type == A_FLOAT)
+      x->pins[i] = atom_getint(argv + i);
+    else
+    {
+      post("error while creating mraagpio");
+      return NULL;
+    }
+  }
   x->x_clock = clock_new(x, (t_method)mraagpio_tick);
   outlet_new(&x->x_obj, gensym("bang"));
-  /* inlet_new(&x->x_obj, &s_list) */
-  /* if (tempo != 0) */
-  /*   mraagpio_tempo(x, unitname, tempo); */
-
+  clock_delay(x->x_clock, 100);
   return (x);
 }
 
@@ -41,13 +50,5 @@ void mraagpio_setup(void)
 {
   mraagpio_class = class_new(gensym("mraagpio"), (t_newmethod)mraagpio_new,
 			     (t_method)mraagpio_free, sizeof(t_mraagpio), 0,
-			     A_DEFFLOAT, A_DEFFLOAT, A_DEFSYM, 0);
-
-  /* class_addbang(mraagpio_class, mraagpio_bang); */
-  /* class_addmethod(mraagpio_class, (t_method)mraagpio_stop, gensym("stop"), 0); */
-  /* class_addmethod(mraagpio_class, (t_method)mraagpio_ft1, gensym("ft1"), */
-  /* 		  A_FLOAT, 0); */
-  /* class_addmethod(mraagpio_class, (t_method)mraagpio_tempo, */
-  /* 		  gensym("tempo"), A_FLOAT, A_SYMBOL, 0); */
-  /* class_addfloat(mraagpio_class, (t_method)mraagpio_float); */
+			     A_GIMME, 0);
 }
