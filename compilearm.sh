cb2b47245da131c62705d2e4afeef430e5cfd35c
nicola montecchio <nicola.montecchio@gmail.com>
2017-10-14 12:44:00 +0200

    slightly more comfortable compilation script

--- compilearm.sh
+++ compilearm.sh
@@ -1,22 +1,43 @@
 # run this from docker using the cross-compiling image thing:
 # docker run -it --rm -v `pwd`:/shared dockcross/linux-armv7 bash
 
+
 cd /shared
 
-# build alsa
-mkdir alsalibs
-cd alsalibs
+# was ALSA already compiled? if not, do it
+if [ ! -f /shared/armlibs/alsa-lib-1.1.4.1/src/.libs/libasound.a ]; then
+    rm -rf armlibs
+    mkdir armlibs
+    echo "building ALSA"
+    cd armlibs
+    wget ftp://ftp.alsa-project.org/pub/lib/alsa-lib-1.1.4.1.tar.bz2
+    tar -jxvf alsa-lib-1.1.4.1.tar.bz2
+    cd alsa-lib-1.1.4.1
+    ./configure
+    make -j4
+fi
 
-wget ftp://ftp.alsa-project.org/pub/lib/alsa-lib-1.1.4.1.tar.bz2
-tar -jxvf alsa-lib-1.1.4.1.tar.bz2
-cd alsa-lib-1.1.4.1
 
-echo "building ALSA"
-./configure
-make -j4
+# make install  # TODO needed by portaudio?
+# echo "building PORTAUDIO"
+# cd /shared/armlibs
+# wget http://www.portaudio.com/archives/pa_stable_v190600_20161030.tgz
+# tar -zxvf pa_stable_v190600_20161030.tgz
+# cd portaudio
+# ./configure --without-oss --with-alsa
+
+
+
+
+
 
 echo "building the listdevices script"
+cd /shared
 $CC -o listdevices -Iarmlibs/alsa-lib-1.1.4.1/include -lm -lpthread -ldl  listdevices.c armlibs/alsa-lib-1.1.4.1/src/.libs/libasound.a
+
+
+
+
 
 echo "scp-ing stuff into chip"
 scp listdevices chip@chip.local:listdevices
--- listdevices.c
+++ listdevices.c
@@ -2,11 +2,12 @@
 /* #include <portaudio.h> */
 #include <alsa/asoundlib.h>
 
-
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
 
   int n, num_audio_devices, num_midi_devices;
+
+   printf("ALSA library version: %s\n",
+          SND_LIB_VERSION_STR);
 
   // TODO re-enable portaudio (or just rewrite directly into alsa?)
 
@@ -25,46 +26,44 @@ int main(int argc, char **argv)
 
   int midi_card = -1;
   int status;
-  while(1)   // loop over sound cards
+  while (1) // loop over sound cards
   {
     char *card_long_name;
     char card_name[128];
     snd_ctl_t *ctl;
     int midi_device = -1;
     status = snd_card_next(&midi_card);
-    if(status < 0)
-    {
-      printf("status: %d - %s -- card: %d\n", status, snd_strerror(status), midi_card);
+    if (status < 0) {
+      printf("status: %d - %s -- card: %d\n", status, snd_strerror(status),
+             midi_card);
       return 1;
     }
-    if(midi_card < 0)
+    if (midi_card < 0)
       break;
     status = snd_card_get_longname(midi_card, &card_long_name);
-    if(status < 0)
-    {
-      printf("status: %d - %s -- card: %d\n", status, snd_strerror(status), midi_card);
+    if (status < 0) {
+      printf("status: %d - %s -- card: %d\n", status, snd_strerror(status),
+             midi_card);
       return 1;
     }
     sprintf(card_name, "hw:%d", midi_card);
     status = snd_ctl_open(&ctl, card_name, 0);
-    if(status < 0)
-    {
-      printf("snd_ctl_open failed with error %d: %s\n", status, snd_strerror(status));
+    if (status < 0) {
+      printf("snd_ctl_open failed with error %d: %s\n", status,
+             snd_strerror(status));
       return 1;
     }
-    while(1)   // loop over devices
+    while (1) // loop over devices
     {
       status = snd_ctl_rawmidi_next_device(ctl, &midi_device);
-      if (status < 0)
-      {
-    	printf("cannot determine device number: %s", snd_strerror(status));
-    	return 1;
+      if (status < 0) {
+        printf("cannot determine device number: %s", snd_strerror(status));
+        return 1;
       }
-      if(midi_device < 0)
-    	break;
+      if (midi_device < 0)
+        break;
       printf(" [%3d]: %s\n", midi_card, card_long_name);
     }
     snd_ctl_close(ctl);
   }
-
 }
